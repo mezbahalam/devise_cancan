d9021afa7ed6ff920270651e9cf0cbe9e6f0c2c2
Mezbah Alam <mezbahalam26@gmail.com>
2015-09-23 00:15:28 +0600

    #delete comments__

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,4 @@ Rails.application.routes.draw do
   devise_for :users
   root 'statuses#index'
   resources :statuses
-  
-
-
-
- 
-  
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
